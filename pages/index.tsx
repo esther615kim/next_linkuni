e7aaf5518fd803060647a66bebf647969e135838
esther615kim <vilchen@naver.com>
2022-11-06 04:38:47 +0900

    setting: add Tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div>Linkuni with Next.js</div>;
+  return (
+    <div className="text-3xl font-bold underline">Linkuni with Next.js</div>
+  );
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
